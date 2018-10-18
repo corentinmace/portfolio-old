8336b5f5b87a2aad7b429f74799800d4f345580b
Corentin MACE <corentin.mace@ynov.com>
2018-10-18 17:16:40 +0200

    tentative raté mdr

--- index.php
+++ index.php
@@ -6,46 +6,62 @@
     <link rel="icon" type="image/png" href="assets/img/favicon.jpg" />
     <link href="/assets/style/aos.css" rel="stylesheet">
 
+
+
+    <script src="particles.js"></script>
+      <script src="app.js"></script>
+
+
     <title>Portfolio - Corentin Macé</title>
   </head>
   <body>
-    <nav id "menu">
-      <div  data-aos="fade-right" data-aos-anchor="menu" >
-        <a href="index.html#accueil">ACCUEIL</a>
-        <a href="index.html#aboutme">A PROPOS</a>
-        <a href="index.html#cv">CURRICULUM VITAE</a>
-        <a href="index.html#contact">CONTACT</a>
-      </div>
-      </nav>
 
-
-
-    <header>
-
-    </header>
-
-
+  <nav id "menu">
+    <div  data-aos="fade-right" data-aos-anchor="menu" >
+      <a href="index.html#accueil">ACCUEIL</a>
+      <a href="index.html#aboutme">A PROPOS</a>
+      <a href="index.html#cv">CURRICULUM VITAE</a>
+      <a href="index.html#contact">CONTACT</a>
+    </div>
+  </nav>
   <main>
-  <div id="accueil">
-    <h1 data-aos="zoom-in" class="page_title">PORTFOLIO</h1>
-  </div>
-
-      <div id="aboutme">
-    <h1 data-aos="zoom-in" class="page_title">A PROPOS</h1>
-      </div>
-        <div id="cv">
-    <h1 data-aos="zoom-in" class="page_title">CURRICULUM VITAE</h1>
-        </div>
-        <div id="contact">
-    <h1 data-aos="zoom-in" class="page_title">CONTACT</h1>
 
 
 
+    <div id="accueil">
+      <h1 data-aos="zoom-in" class="page_title">PORTFOLIO</h1>
+    </div>
+    <div id="aboutme">
+      <h1 data-aos="zoom-in" class="page_title">A PROPOS</h1>
+    </div>
+    <div data-aos="zoom-in" class="aboutme">
+        	<h1>Corentin MACE</h1>
 
-</div>
-</main>
+			<p id="particles-js" data-aos="fade-right">Je m'appelle Corentin Macé, j'ai 18 ans !<br />
+				J'habite à Berre l'etang (13)<br />
+			J'étudie actuellement à <a href="https://www.ynov-aix.fr/">Ynov Aix-en-Provence</a>
+		Je compte me spécialiser dans la sécurité informatique et dans le hack<br />
+		J'ai des bases en HTML/CSS, en Arduino<br />
+		Je suis un passioné d'informatique, de skate et de photographie !<br />
+		Je suis aussi bassiste dans un groupe de stoner-rock, qui est appellé <a href="https://www.facebook.com/flyindistortion/?ref=bookmarks">Flyin' Distortion</a>
+	</p>
+    </div>
+    <div id="cv">
+      <h1 data-aos="zoom-in" class="page_title">CURRICULUM VITAE</h1>
+    </div>
+    <div class="cv">
 
-    <footer>
+    </div>
+    <div id="contact">
+      <h1 data-aos="zoom-in" class="page_title">CONTACT</h1>
+    </div>
+    <div class="contact">
+
+    </div>
+  </main>
+
+
+  <footer>
     <p>Copryright &copy; | Corentin Macé - 2018 | All Right Reserved</p>
   </footer>
 
