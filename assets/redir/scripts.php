d351a0bd6184ab9d05b3d14059dcb3d0c873c8c4
Mace <corentin.mace@ynov.com>
2018-12-02 13:23:45 +0100

    v3

---
+++ assets/redir/scripts.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="assets/scripts/trigger-hamburger.js">
+
+</script>
+<script type="text/javascript" src="assets/scripts/scripts.js">
+
+</script>
--- index.php
+++ index.php
@@ -2,203 +2,33 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="assets/style/main.css">
-    <link rel="icon" type="image/png" href="assets/img/favicon.jpg" />
-    <link href="/assets/style/aos.css" rel="stylesheet">
-
-          <title>Portfolio - Corentin Macé</title>
+    <title>Portfolio - Corentin Macé</title>
+    <link rel="stylesheet" href="assets/style/style.css">
+<script src="//code.jquery.com/jquery-3.1.1.slim.min.js"></script>
   </head>
-  <body>
+    <body>
+      <header>
 
-  	<nav id "menu">
-    	<div  data-aos="fade-right" data-aos-anchor="menu" >
-      	<a href="index.html#accueil">ACCUEIL</a>
-      	<a href="index.html#aboutme">A PROPOS</a>
-      	<a href="index.html#cv">CURRICULUM VITAE</a>
-				<a href="index.html#projects">PROJETS</a>
-				<a href="index.html#contact">CONTACT</a>
+        <button class="hamburger hamburger--emphatic toggle-button" id="toggle" type="button">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </button>
+        <div class="navbar-header">
 
-    	</div>
-  	</nav>
-
-  <main>
-
-
-
-    <div id="accueil">
-
-			      <div id="particles-js"></div>
-       <h1  data-aos="zoom-in" data-aos-anchor="none" class="page_title">PORTFOLIO</h1>
-		 	</div>
-    <div id="aboutme">
-
-
-      <h1 data-aos="zoom-in" class="page_title">A PROPOS</h1>
-    </div>
-    <div data-aos="zoom-in" class="aboutme">
-        	<h1>Corentin MACE</h1>
-
-			<p onscroll="block" data-aos="fade-right">Je m'appelle Corentin Macé, j'ai 18 ans !<br />
-				J'habite à Berre l'etang (13)<br />
-			J'étudie actuellement à <a href="https://www.ynov-aix.fr/">Ynov Aix-en-Provence</a>
-		Je compte me spécialiser dans la sécurité informatique et dans le hack<br />
-		J'ai des bases en HTML/CSS, en Arduino<br />
-		Je suis un passioné d'informatique, de skate et de photographie !<br />
-		Je suis aussi bassiste dans un groupe de stoner-rock, qui est appellé <a  target="_blank"href="https://www.facebook.com/flyindistortion/?ref=bookmarks">Flyin' Distortion</a>
-    <br />Je crée ce site en vu de mon projet de fin d'année en cour d'HTML/CSS à Ynov
-	</p>
-
-    </div>
-    <div id="cv">
-
-      <h1 data-aos="zoom-in" class="page_title">CURRICULUM VITAE</h1>
-    </div>
-
-
-      <!-- CV -->
-    <div class="cv">
-      <div class="cvblock">
-
-
-        <div class="headercvleft"><h1>MACE Corentin</h1><h4>né le 27 juin 2000</h4>
-          <br /><p>CURRICULUM VITAE</p></div>
-
-        <div class="headercvright"><div class="aboutmecv">
-          <h1>A propos de moi</h1></div>
-          <ul>
-            <li>- Passioné d'informatique depuis mon plus jeune age</li>
-            <li>- Pratique le skateboard depuis 3 ans</li>
-            <li>- Photographe amateur</li>
-            <li>- Bassiste dans un groupe de rock</li>
-            <li>- Passioné de jeux vidéos depuis mon plus jeune age</li>
-            <li>- Dessine depuis juin 2017</li>
-          </ul>
         </div>
-            <img class="photocv" src="assets/img/idpic.jpg" alt="">
+        <nav class="overlay" id="overlay">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Curriculum Vitae</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</nav>
+    </header>
+    <div class="container">
 
-
-     <div class="barrelatdroite">
-           <div class="barreblanche">
-
-            <h1>COMPTENCES</h1>
-           </div>
-
-             <p class="principal-line">Creativité</p>
-             <progress value="60" max="100">60%</progress>
-             <p class="principal-line">Windows</p>
-             <progress value="90" max="100">90%</progress>
-             <p class="principal-line">Microsoft Office</p>
-             <progress value="80" max="100">80%</progress>
-             <p class="principal-line">Maintenance informatique</p>
-             <progress value="80" max="100">80%</progress>
-             <p class="principal-line">Internet</p>
-             <progress value="95" max="100">95%</progress>
-             <p class="principal-line">HTML-CSS</p>
-             <progress value="65" max="100">65%</progress>
-             <p class="principal-line">Photoshop</p>
-             <progress value="60" max="100">60%</progress>
-
-           <div class="barreblanche">
-
-             <h1>LANGUES</h1>
-           </div>
-
-             <p class="principal-line">Anglais</p>
-             <progress value="85" max="100">85%</progress>
-             <p class="principal-line">Espagnol</p>
-             <progress value="45" max="100">45%</progress>
-             <p class="principal-line">Japonnais</p>
-             <progress value="10" max="100">10%</progress>
-           </div>
-
-     <div class="barrelatgauche">
-      <div class="studiesg">
-
-      <img src="/assets/ico/studies.png" alt="">
-       <p>2018 - Aujourd'hui</p>
-       <p>2015 - 2018</p>
-       <p>2011 - 2015</p></div>
-
-      <div class="expprog">
-      <img src="/assets/ico/exp.png" alt="">
-       <p>2018 - Aujourd'hui</p>
-       <p>2015 - Aujourd'hui</p>
-       <p>2015</p></div>
-     </div>
-
-        <div class="formation">
-          <h1>Formation</h1>
-          <ul>
-            <li>Ynov Digital School - informatique | Aix en Provence</li>
-            <li>Lycée Pierre Mendes France - Bac STI2D | Vitrolles</li>
-            <li>College Fernand Leger - Brevet | Berre L'etang</li>
-          </ul>
-        </div>
-        <div class="exppro">
-          <h1>Experience Profesionnelle</h1>
-          <ul>
-            <h3>Co-Directeur de startup</h3>
-            <li>Hacking Challenge</li>
-            <li class="commentaire">Co-Directeur d'une startup dans le contexte des Y-Days à Ynov Digital School</li>
-
-            <h3>Réparation d'ordinateur</h3>
-            <li class="commentaire">Aide et réparation d'ordinateur pour des amis ou des recommandations</li>
-
-            <h3>Stage de 3°</h3>
-            <li>InfoBuro</li>
-            <li class="commentaire">Stage dans une entreprise pour le collège</li>
-          </ul>
-        </div>
-
-
-
-
-       </div>
-
-    <button type="button" name="cvfr"><a href="/assets/files/cvfr.pdf" download="CV Français MACE Corentin">Télécharger mon CV</a></button>
-    <button type="button" name="cven"><a href="/assets/files/cven.pdf" download="CV English MACE Corentin">Download my CV</a> </button>
- </div>
-
-      <!-- Fin CV -->
-		<div id="projects">
-				<h1 data-aos="zoom-in" class="page_title">PROJETS</h1>
-		</div>
-		<div class="projects">
-      <h1>C'est ici que vous retrouverez mes projets en cour et finis</h1>
-<div class="cards">
-<div class="github-card" data-github="corentinmace" data-width="400" data-height="" data-theme="default"></div>
-<script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
-    <h2>Arduino - Thermometre sur afficheur 4 digit</h2>
-    <p>Ceci est mon premier projet que j'ai fais sur Arduino, il affiche simplement la tempature d'une sonde sur un afficheur 4 digit</p>
-    <div class="github-card" data-github="corentinmace/temp-4digit-arduino" data-width="400" data-height="" data-theme="default"></div>
-<script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
 </div>
-		</div>
-
-    <div id="contact">
-
-      <h1 data-aos="zoom-in" class="page_title">CONTACT</h1>
-    </div>
-    <div class="contact">
-
-    </div>
-  </main>
-
-
-  <footer>
-    <p>Copryright &copy; | Corentin Macé - 2018 | All Right Reserved</p>
-  </footer>
-
-
-   <script src="/assets/script/particles.min.js"></script>
-   <script src="/assets/script/particles.js"></script>
-    <script src="/assets/script/app.js"></script>
-    <script src="/assets/script/aos.js"></script>
-    <script src="/assets/script/jquery.js"></script>
-    <script type="text/javascript" src="/assets/script/smooth.js"></script>
-    <script>
-  			AOS.init(3000);
-		</script>
-
-  </body>
+      </header>
+        <?php include "assets/redir/scripts.php" ?>
+    </body>
 </html>
