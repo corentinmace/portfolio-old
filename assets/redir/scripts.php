7c0ca284afd9191af7c1fb9e361c8757e61c2c5a
Corentin MACE <corentin.mace@ynov.com>
2018-12-27 23:43:23 +0100

    kek commit

--- assets/redir/scripts.php
+++ assets/redir/scripts.php
@@ -1,6 +1,8 @@
+<script type="text/javascript" src="assets/scripts/aos.js"></script>
 <script type="text/javascript" src="assets/scripts/trigger-hamburger.js">
 
 </script>
-<script type="text/javascript" src="assets/scripts/scripts.js">
-
+<script type="text/javascript" src="assets/scripts/scripts.js"></script>
+<script>
+  AOS.init();
 </script>
--- contact.php
+++ contact.php
@@ -13,36 +13,36 @@
         <button class="hamburger hamburger--emphatic toggle-button" id="toggle" type="button">
           <span class="hamburger-box">
             <span class="hamburger-inner"></span>
-					</span>
           </button>
         <div class="navbar-header">
-
         </div>
         <nav class="overlay" id="overlay">
 						<a href="https://corentinmace.com"><img  class="logo" src="assets/images/icons/logo-white.png" alt="logo"></a>
-  <ul>
-    <li><a href="index.php">HOME</a></li>
-    <li><a href="about.php">ABOUT</a></li>
-    <li><a href="cv.php">CURRICULUM VITAE</a></li>
-    <li><a href="contact.php">CONTACT</a></li>
-  </ul>
-</nav>
+  								<ul>
+    						<li><a href="index.php">HOME</a></li>
+    					<li><a href="about.php">ABOUT</a></li>
+    					<li><a href="cv.php">CURRICULUM VITAE</a></li>
+    					<li><a href="contact.php">CONTACT</a></li>
+  					</ul>
+					</nav>
 
-    <div class="container">
+    		<div class="container">
 
-</div>
-			<section id="home">
-				<a href="http://corentinmace.com"><img  class="logo" src="assets/images/icons/logo.png" alt="logo"></a>
-				<div data-aos="fade-down" class="middle">
-							<h1>WELCOME ON MY PORTFOLIO</h1>
 				</div>
+				<section id="contact">
+					<a href="http://corentinmace.com"><img  class="logo" src="assets/images/icons/logo.png" alt="logo"></a>
+					<div data-aos="fade-down" class="middle">
+								<h1>CONTACT</h1>
+								<div class="scrolldown">
+									<h2><img class="animFleche" src="assets/images/icons/arrowdown.png" alt="">	SCROLL DOWN !<img class="animFleche" src="assets/images/icons/arrowdown.png" alt=""></h2>								</div>
+							</div>
+						</section>
+    		</header>
 
-
-			</section>
-      </header>
 			<main>
+					<section id="block1">
 
+					</section>
 			</main>
         <?php include "assets/redir/scripts.php" ?>
     </body>
-</html>
